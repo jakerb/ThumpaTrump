1f102e647ad1ab352dc28c74def6b21b6613b229
Jake Bown <jakebown@gmail.com>
2016-02-25 23:54:30 +0000

    VW!!

--- platforms/android/src/com/phonegap/thumpatrump/MainActivity.java
+++ platforms/android/src/com/phonegap/thumpatrump/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends CordovaActivity
     {
         super.onCreate(savedInstanceState);
         // Set by <content src="index.html" /> in config.xml
+
         loadUrl(launchUrl);
     }
 }
